156882ce3dcfdfaec2eb06debe9001ea83d77478
jake watters <jerkey@g-cipher.net>
2023-08-08 01:14:49 -0700

    send magne-charge J1773 signals on command

--- j1850-to-serial.ino
+++ j1850-to-serial.ino
@@ -1,33 +1,25 @@
-
 #include "j1850vpw.h"
 
-#define TX 9
+#define TX 11
 #define RX 10
+#define CPCT 13 // when this goes high, it pulls the yellow J-clip up to +12v
 
 void handleError(J1850_Operations op, J1850_ERRORS err);
 J1850VPW vpw;
 
 void setup()
 {
-    Serial.begin(9600);           // start serial port
-    Serial.println("OK");
-    delay(300);
-    if (Serial.available() > 0) {
-      switch(Serial.read()) {
-        case '0': Serial.begin(115200); break;
-        case '1': Serial.begin(56800); break;
-        case '2': Serial.begin(38400);
-        default:  Serial.begin(19200); break;
-      }
-    }
+    pinMode(CPCT, OUTPUT); // it's connected to a 2N7000 so we need to control the gate, there is no pulldown resistor
+    Serial.begin(115200);           // start serial port
+    Serial.println("j1850-to-serial.ino");
     vpw.onError(handleError); // listen for errors
     vpw.init(RX, TX);         // init transceiver
+    Serial.setTimeout(1000); // default timeout for Serial.parseInt() is 1000 ms
 }
 
 void loop()
 {
     static uint8_t buff[BS];                   // buffer for read message
-    static uint8_t sendBuff[2] = {0x01, 0x02}; // message to send, CRC will be read and appended to frame on the fly
 
     uint8_t dataSize; // size of message
 
@@ -53,13 +45,43 @@ void loop()
             s += String(pData[i], HEX);
         }
 
-        Serial.println(s);
+        Serial.println(String(millis())+"	"+s);
+    }
+    int16_t commandCode = Serial.parseInt(); // read a number from the serial port
+    if (commandCode == 12) {
+      Serial.println("12: turning on CPCT yellow J-clip to 12v");
+      digitalWrite(CPCT, HIGH);
+    }
+    if (commandCode == 10) {
+      Serial.println("10: SHUTTING OFF CPCT yellow J-clip to 12v");
+      digitalWrite(CPCT, LOW);
+    }
+    if (commandCode == 1) {
+      Serial.println("1: sending BUS WAKEUP, CHARGER WAKEUP messages");
+      static uint8_t bus_wakeup[4] = {0x88, 0xFE, 0xF2, 0x82}; // message to send, CRC will be read and appended to frame on the fly
+      vpw.send(bus_wakeup, 4);
+      delay(154);
+      static uint8_t charger_status[5] = {0x88, 0x73, 0xF2, 0x23, 0x00};
+      vpw.send(charger_status, 5);
+    }
+    if (commandCode == 2) {
+      Serial.println("2: sending BUS WAKEUP, CHARGER INIT messages");
+      static uint8_t bus_wakeup[4] = {0x88, 0xFE, 0xF2, 0x82}; // message to send, CRC will be read and appended to frame on the fly
+      vpw.send(bus_wakeup, 4);
+      delay(154);
+      static uint8_t charger_status[5] = {0x88, 0x73, 0xF2, 0x23, 0x01};
+      vpw.send(charger_status, 5);
+    }
+    if (commandCode == 3) {
+      Serial.println("3: sending CHARGER INIT, CHARGER CAPABILITY messages");
+      static uint8_t charger_status[5] = {0x88, 0x73, 0xF2, 0x23, 0x01};
+      vpw.send(charger_status, 5);
+      delay(154);
+      static uint8_t charger_capability[8] = {0xA8, 0x73, 0xF2, 0x24, 0x02, 0x58, 0x00, 0x00}; // 6000 watts
+      vpw.send(charger_capability, 8);
     }
 
-    // write simple message to bus
-    vpw.send(sendBuff, sizeof(sendBuff));
-
-    delay(1000);
+// delay(1000);
 }
 
 
@@ -71,7 +93,7 @@ void handleError(J1850_Operations op, J1850_ERRORS err)
         return;
     }
 
-    String s = op == J1850_Read ? "READ " : "WRITE ";
+    String s = String(millis()) +"	"+ (op == J1850_Read ? "READ " : "WRITE ");
     switch (err)
     {
     case J1850_ERR_BUS_IS_BUSY:
